eb7e797af1ff49cb463d853d2fc836b89e7d6c16
Mark Young <mark@tip2tail.scot>
2019-11-29 17:02:43 +0100

    Files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoIPUpdateTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
